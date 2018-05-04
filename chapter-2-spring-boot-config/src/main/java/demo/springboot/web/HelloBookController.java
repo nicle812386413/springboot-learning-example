321ff8ce82a8b998548fcb2421ad21b2b104226c
fjk <812386413@qq.com>
2018-05-04 23:30:06 +0800

    GetMapping/PostMapping/RequestMapping生成的swagger2Doc文档

--- chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
+++ chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
@@ -4,6 +4,7 @@ import demo.springboot.config.BookComponent;
 import demo.springboot.config.BookProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Spring Boot Hello案例
  *
  * Created by bysocket on 26/09/2017.
+ * http://localhost:8080/swagger-ui.html支持swagger查看接口文档
  */
 @RestController
 public class HelloBookController {
@@ -27,7 +29,7 @@ public class HelloBookController {
                 + bookProperties.getName() + " ！";
     }
 
-    @GetMapping("/hi")
+    @PostMapping("/hi")
     public String sayHi() {
         return "hi， " + bookComponent.getWriter() + " is writing "
                 + bookComponent.getName() + " ！";
