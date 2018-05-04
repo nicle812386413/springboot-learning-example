14af7f6d33d3e2a1804e1cd346059708faab2e12
fjk <812386413@qq.com>
2018-05-04 22:53:33 +0800

    properties注入方式，和环境

--- chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
+++ chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
@@ -1,8 +1,10 @@
 package demo.springboot.web;
 
+import demo.springboot.config.BookComponent;
 import demo.springboot.config.BookProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,9 +18,24 @@ public class HelloBookController {
     @Autowired
     BookProperties bookProperties;
 
+    @Autowired
+    private BookComponent bookComponent;
+
     @GetMapping("/book/hello")
     public String sayHello() {
         return "Hello， " + bookProperties.getWriter() + " is writing "
                 + bookProperties.getName() + " ！";
+    }
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "hi， " + bookComponent.getWriter() + " is writing "
+                + bookComponent.getName() + " ！";
+    }
+
+    @RequestMapping("/say")
+    public String sayLanguage() {
+        return "你好， " + bookComponent.getWriter() + " is writing "
+                + bookComponent.getName() + " ！";
     }
 }
